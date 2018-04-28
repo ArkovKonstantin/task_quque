fa17d42d581e2c17f66aa9ce923027ad504884b0
k1lLr3AlL <31995128+k1lLr3AlL@users.noreply.github.com>
2018-04-28 21:10:35 +0300

    add class Task, fix time, fix func run

--- server.py
+++ server.py
@@ -4,97 +4,117 @@ import pickle
 from datetime import datetime
 
 
+class Task:
+    def __init__(self, task_id, length, data, status='handle', time=datetime.now()):
+        self.task_id = task_id
+        self.length = length
+        self.data = data
+        self.status = status
+        self.time = time
+
+    @staticmethod
+    def check_time(queue_dict):
+        dt = '5'  # 5 минут в секундах
+        for queue in queue_dict.values():
+            for task in queue:
+                if task.status == 'perform':
+                    if str((datetime.now() - task.time).seconds) >= dt:
+                        queue.remove(task)
+                        task.status = 'handle'
+                        queue.append(task)
+        return queue_dict
+
+    @staticmethod
+    def write(file_name, data):
+        with open(file_name, 'wb') as f:
+            pickle.dump(data, f)
+
+    @staticmethod
+    def read(file_name):
+        with open(file_name, 'rb') as f:
+            new_data = pickle.load(f)
+        return new_data
+
+
 class Server:
     count_task = 0
-    dt = '300'  # 5 минут в секундах
 
     def __init__(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sock.bind(('127.0.0.1', 5555))  # устанавливаем host и port
         self.sock.listen(1)  # устанавливаем режим прослушивания
-
+        self.dict_command = {'ADD': self._add, 'IN': self._in, 'ACK': self._ack, 'GET': self._get}
         self.queue_dict = collections.defaultdict(list)
         self.sock.setblocking(0)
 
     def run(self):
         try:
             while True:
-                while True:
-                    try: conn, addr = self.sock.accept()
-                    except socket.error:
-                        break
-                    else:
-                        data = conn.recv(1000000)
-                        arguments = data.decode('utf-8').split(' ')
-                        command = arguments.pop(0)
-                        try:
-                            self.queue_dict.update(self.read('data'))  # Чтение из файла
-                        except EOFError:
-                            pass
-
-                        conn.send(eval('self.' + command)(*arguments))
-                        conn.close()
+                try:
+                    conn, addr = self.sock.accept()
+                except socket.error:
+                    continue
+                else:
+                    data = conn.recv(1000000)
+                    arguments = data.decode('utf-8').split(' ')
+                    command = arguments.pop(0)
+                    try:
+                        self.queue_dict.update(Task.read('data'))  # Чтение из файла
+                        self.queue_dict = Task.check_time(self.queue_dict)
+                        Task.write('data', self.queue_dict)  # Запись в Файл
+                    except EOFError:
+                        pass
+                    conn.send(self.dict_command[command](*arguments))
+                    conn.close()
 
         except KeyboardInterrupt:
             return None
 
-    def ADD(self, queue, length, data):
+    def _add(self, queue, length, data):
         try:  # Чтение счетчика ID из файла
-            self.count_task = self.read('count')
+            self.count_task = Task.read('count')
             self.count_task += 1
         except EOFError:
             self.count_task += 1
 
-        self.write('count', self.count_task)  # Запись счетсика ID в Файл
-
-        task = {'id': str(self.count_task), 'length': length, 'data': data, 'status': 'handle'}
-
+        Task.write('count', self.count_task)  # Запись счетсика ID в Файл
+        task = Task(str(self.count_task), length, data)
         self.queue_dict[queue].append(task)
-        self.write('data', self.queue_dict)  # Запись в Файл
+        Task.write('data', self.queue_dict)  # Запись в Файл
 
-        return bytes(str(self.queue_dict[queue][-1]['id']), 'utf-8')
+        return bytes(str(self.queue_dict[queue][-1].task_id), 'utf-8')
 
-    def IN(self, queue, task_id):
+    def _in(self, queue, task_id):
         for task in self.queue_dict[queue]:
-            if task['id'] == task_id:
+            if task.task_id == task_id:
                 return b'YES'
         return b'NO'
 
-    def GET(self, queue):
+    def _get(self, queue):
         if self.queue_dict.get(queue):
-            for task in self.queue_dict[queue]:
-                if task['status'] == 'perform':
-                    if str(task['time'] - datetime.now()) >= self.dt:
-                        self.queue_dict[queue].remove(task)
-                        task['status'] = 'handle'
-                        self.queue_dict[queue].append(task)
 
-                if task['status'] == 'handle':
-                    task['status'] = 'perform'
-                    task['time'] = datetime.now()
-                    self.write('data', self.queue_dict)  # Запись в Файл
-                    return bytes(' '.join(list(task.values())[:len(task.values()) - 2]), 'utf-8')
+            for task in self.queue_dict[queue]:
+                if task.status == 'handle':
+                    task.status = 'perform'
+                    task.time = datetime.now()
+                    print('task', task)
+                    Task.write('data', self.queue_dict)  # Запись в Файл
+
+                    return bytes('{} {} {}'.format(task.task_id, task.length, task.data), 'utf-8')
+            else:
+                return b'NONE'  # Вернет None если все задания исполняются
 
         else:
             return b'NONE'
 
-    def ACK(self, queue, task_id):
+    def _ack(self, queue, task_id):
         for task in self.queue_dict[queue]:
-            if task['id'] == task_id:
+            if task.task_id == task_id:
                 self.queue_dict[queue].remove(task)
-                self.write('data', self.queue_dict)  # Запись в Файл
+                Task.write('data', self.queue_dict)  # Запись в Файл
                 return b'YES'
         return b'NO'
-
-    def read(self, file_name):
-        with open(file_name, 'rb') as f:
-            new_data = pickle.load(f)
-        return new_data
-
-    def write(self, file_name, data):
-        with open(file_name, 'wb') as f:
-            pickle.dump(data, f)
 
 
 if __name__ == '__main__':
